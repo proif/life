30c460145043a9a9ca2feec36fcae5a0d4987a97
admin <admin@dstr-util-cld-s.in.storage.sakura.ad.jp>
2014-04-08 10:45:55 +0900

    modified lsof -p processid

--- chk_cn_port.sh
+++ chk_cn_port.sh
@@ -5,6 +5,7 @@ echo "$(ps auxww |grep dss |grep -v grep |awk '{print $2 "/" $NF}' |awk -F/ '{pr
 for i in $rcvPorts
 do
   pId=$(netstat -antp |grep :${i} |head -n 1 |awk '{print $(NF)}' |grep -o [0-9]*)
-  sed -i "s/\($pId\)\( : [a-z0-9-]*\)/\1\2 : ${i}/" $TMP
+  cntFile=$(lsof -p $pId |wc -l)
+  sed -i "s/\($pId\)\( : [a-z0-9-]*\)/\1\2 : ${i} : ${cntFile}/" $TMP
 done
 cat $TMP ; \rm -f $TMP
