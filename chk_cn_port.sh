417645321a90833fe25dd8867d9277b39faead6b
admin <admin@dstr-util-cld-s.in.storage.sakura.ad.jp>
2014-04-08 12:09:08 +0900

    fix "grep write error Broken pipe"

--- chk_cn_port.sh
+++ chk_cn_port.sh
@@ -4,8 +4,8 @@ TMP=$(mktemp) ; rcvPorts="8085 8086 8087"
 echo "$(ps auxww |grep dss |grep -v grep |awk '{print $2 "/" $NF}' |awk -F/ '{print $1 " : "$NF}' |sed -e "s/.cfg//")" > $TMP
 for i in $rcvPorts
 do
-  pId=$(netstat -antp |grep :${i} |head -n 1 |awk '{print $(NF)}' |grep -o [0-9]*)
+  pId=$(netstat -antp 2>/dev/null |grep ":${i}" 2>/dev/null |head -n 1 2>/dev/null |awk '{print $(NF)}' 2>/dev/null |grep -oE "[0-9]*" 2>/dev/null)
   cntFile=$(lsof -p $pId |wc -l)
   sed -i "s/\($pId\)\( : [a-z0-9-]*\)/\1\2 : ${i} : ${cntFile}/" $TMP
 done
-cat $TMP |sort -n -t ':' -k 3 ; \rm -f $TMP
+cat $TMP |sort -n -t ':' -k 3 |tee ; \rm -f $TMP
