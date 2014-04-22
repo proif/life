22f1eeafb983b45c4613cdb10e97239e113cd44b
root <root@dstr-util-cld-s.in.storage.sakura.ad.jp>
2014-04-22 14:58:58 +0900

    modified code

--- chk_raid.sh
+++ chk_raid.sh
@@ -3,13 +3,13 @@
 megaClicmd=$(which MegaCli)
 ldInfo=$($megaClicmd -ldinfo -l0 -a0)
 ldState=$(echo "$ldInfo" |grep -i State |awk -F ': ' '{print $NF}')
-echo $ldState
+#echo $ldState
 #State               : Optimal
 
 if [ X${ldState} = XOptimal ]; then
-  retCode=0
+  retCode=100
 else
-  retCode=1
+  retCode=110
 fi
 
 /usr/local/bin/zabbix_trap.sh megacli.raid.state $retCode
